d6e39cae0a9dd1fc12cd810403c08123085a026e
Pratik Rajbhandari <r.pratik013@gmail.com>
2022-02-27 10:02:36 +0545

    Add meetup form

---
+++ pages/new-meetup/index.js
@@ -0,0 +1,11 @@
+import NewMeetUpForm from "../../components/meetups/NewMeetupForm";
+
+function NewMeetupPage() {
+  function addMeetupHandler(enteredMeetupData) {
+    console.log(enteredMeetupData);
+  }
+
+  return <NewMeetUpForm onAddMeetup={addMeetupHandler} />;
+}
+
+export default NewMeetupPage;
