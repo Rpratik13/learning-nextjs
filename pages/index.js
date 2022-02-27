2befbff536cd4b0bce1773254414463060b5ce2e
Pratik Rajbhandari <r.pratik013@gmail.com>
2022-02-27 09:23:38 +0545

    Add meetup list

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,24 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "First Meetup",
+    image: "https://wallpaperaccess.com/full/359168.jpg",
+    address: "Some address",
+    description: "This is first meetup",
+  },
+  {
+    id: "m2",
+    title: "Second Meetup",
+    image: "https://wallpaperaccess.com/full/359168.jpg",
+    address: "Some address",
+    description: "This is second meetup",
+  },
+];
+
 function HomePage() {
-  return <h1>The Home Page</h1>;
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
 }
 
 export default HomePage;
